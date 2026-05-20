0b694f6c0adbb356ac6089159c0184d9f3984fbf
bala-santoshi <balasantoshi121@gmail.com>
2026-05-20 11:49:37 +0530

    Add files via upload
    Added 4to1 MUX files

---
+++ mux4to1.v
@@ -0,0 +1,29 @@
+
+module mux4to1 (Y,i0,i1,i2,i3,s1,s0);
+output Y;
+input i0,i1,i2,i3;
+input s1,s0;
+
+//Logic equation for output
+assign Y =(~s1&~s0&i0)|(~s1&s0&i1)|(s1&~s0&i2)|(s1&s0&i3);
+endmodule
+
+////////////////////////////////
+module TB_mux();
+wire y;
+reg s0,s1,i0,i1,i2,i3;
+mux4to1 m1(y,i0,i1,i2,i3,s1,s0);
+initial 
+begin
+
+i0=1'b1;i1=1'b0;i2=1'b1;i3=1'b0;
+s0=1'b0;s1=1'b0;
+#100;
+s0=1'b1;s1=1'b1;
+#100;
+s0=1'b1;s1=1'b0;
+#100;
+s0=1'b0;s1=1'b1;
+
+end
+endmodule
